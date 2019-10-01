fb06705152ead8ed50385c06a1838b9884e6d84b
Black_Parmenides <mkhumasmanga@gmail.com>
2019-10-01 04:39:06 +0200

    Urls Mapping #5

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path("articles/<int:article_id>", views.reader, name='articles'),
-    path(r'^(?P<genre_name>\w+)'', views.genres, name='genres'),
+    url(r'^(?P<genre_name>\w+)', views.genres, name='genres'),
     ]
