2ed71da2e83c65bb5eb61aace3847573c351e5fc
Black_Parmenides <mkhumasmanga@gmail.com>
2019-10-01 04:27:29 +0200

    Urls Mappings #3 --fun ey :(

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("articles/<int:article_id>", views.reader, name='articles'),
-    path("articles/(?P<genre_name>\w+)", views.genres, name='genres'),
+    path("(?P<genre_name>\w+)", views.genres, name='genres'),
     ]
