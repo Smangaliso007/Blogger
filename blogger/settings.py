ff5db1676409e5990ce166691a840097b1d5e60a
Black_Parmenides <mkhumasmanga@gmail.com>
2019-10-03 10:47:01 +0200

    SSTATIC

--- blogger/settings.py
+++ blogger/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(parent_dir, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ()
 
-MEDIA_ROOT = os.path.join(parent_dir, 'staticfiles/static/MEDIA/')
+MEDIA_ROOT = os.path.join(STATIC_URL, 'MEDIA/')
 MEDIA_URL = '/staticfiles/static/MEDIA'
 # Heroku: Update database configuration from $DATABASE_URL.
 
