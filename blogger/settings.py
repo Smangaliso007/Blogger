da4de0c9149af2215851b98d681d638fd2a9dc79
Black_Parmenides <mkhumasmanga@gmail.com>
2019-10-04 10:19:14 +0200

    k

--- blogger/settings.py
+++ blogger/settings.py
@@ -140,7 +140,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ()
 
-COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
 MEDIA_URL = '/Media/'
