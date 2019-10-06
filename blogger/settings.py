a289ffd18081b92acfbe23dab5b09fb82336b3a4
Black_Parmenides <mkhumasmanga@gmail.com>
2019-10-06 16:31:39 +0200

    Don't need Staticfiles_Dirs??

--- blogger/settings.py
+++ blogger/settings.py
@@ -147,9 +147,7 @@ USE_TZ = True
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
